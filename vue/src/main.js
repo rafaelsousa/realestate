6c6462fde8c5231680695cbd21a1b0228683c6ae
Rafael de Andrade Sousa <rafael.andrade@tendermint.com>
2022-01-01 03:08:12 -0500

    Add prime vue support

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import vueLib from '@starport/vue'
+import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 app.config.globalProperties._depsLoaded = true
-app.use(store).use(router).use(vueLib).mount('#app')
+app.use(store).use(router).use(vueLib).use(PrimeVue).mount('#app')
